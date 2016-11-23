50a8ad8f8089966b4ca7f226a9aa0fbff05eeda7
Ben Cook <b@bencook.ca>
2016-11-23 01:49:41 -0800

    Switch to request-promise

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,45 +1,33 @@
-import btoa from 'btoa'
-import __fetch from 'fetch-ponyfill'
-const {fetch: _fetch, Headers, Request} = __fetch()
-import formurlencoded from 'form-urlencoded'
+import * as Promise from 'bluebird'
+import * as rp from 'request-promise'
 
-import {DateRange, JQL} from '../types'
+import {JQL} from 'jqllib'
 
 const MP_API_JQL_URI = 'https://mixpanel.com/api/2.0/jql/'
 
-let _headers = new Headers()
-_headers.set('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+let _apiKey: string = null
 
 /**
  * Sets the Mixpanel secret key to use for requests.
- * Note: jqllib is stateful; this key is shared across all usages.
+ * Note: jqllib-fetch is stateful; this key is shared across all usages.
  */
-// str -> ()
-export function setApiKey(key: string) {
-    _headers.set('Authorization', `Basic ${btoa(key + ':')}`)
+export function setApiKey(key: string): void {
+    _apiKey = key
 }
 
 /**
  * Returns the results of running the `jql` at Mixpanel.
  * Note: the Mixpanel API limits running time to a few minutes.
  */
-// str -> Promise<?>
-function mpFetch<T>(jql: JQL<T>) {
-    if (!_headers.has('Authorization')) { throw 'API key not set' }
+export function fetch<T>(jql: JQL<T>): Promise<T> {
+    if (!_apiKey) { throw 'API key not set' }
 
     jql = `function main() { ${jql} }`
 
-    const data = formurlencoded({
-        script: jql,
-        params: JSON.stringify({})
+    return rp.post(MP_API_JQL_URI, {
+        form: {script: jql, params: JSON.stringify({})},
+        headers: {'Cache-control': 'no-cache'},
+        auth: {user: _apiKey}
     })
-
-    return _fetch(new Request(MP_API_JQL_URI), {
-        method: 'POST',
-        headers: _headers,
-        body: data,
-        cache: 'no-cache'
-    })
-    .then(r => r.json())
+    .then(JSON.parse)
 }
-export const fetch = mpFetch
